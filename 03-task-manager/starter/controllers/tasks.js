dae6d96bf8bfb7f690bfcbc6a64e0b809258e173
Radiatechs-Mayank <103631131+Radiatechs-Mayank@users.noreply.github.com>
2022-07-25 11:19:52 +0530

    commit 25/7/22

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,24 +1,55 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req,res) => {
-    res.send('get all tasks')
+const getAllTasks = async (req,res) => {
+    try{
+        const tasks = await Task.find({})
+        res.status(200).send(tasks)
+    }
+    catch(error){
+        res.status(500).send({msg: error})
+    }
 }
 
 const addTask = async (req,res) => {
+    try{
     const task = await Task.create(req.body);
     res.status(201).send(task)
+    }
+    catch(error){
+        res.status(500).send({msg: error})
+    }
 }
 
-const getTask = (req,res) => {
-    res.send('get single task')
+const getTask = async (req,res) => {
+    try{
+        const {id:taskId} = req.params  // destructuring
+        const task = await Task.findOne({_id: taskId})
+        if(!task){
+            return res.status(404).send({msg: 'No such task with id: ' + taskId + ' exists'})
+        }
+        res.status(200).send(task)
+    }
+    catch(error){
+        res.status(500).send({msg: error})
+    }
 }
 
 const updateTask = (req,res) => {
     res.send('update task')
 }
 
-const deleteTask = (req,res) => {
-    res.send('delete task')
+const deleteTask = async (req,res) => {
+    try{
+        const {id:taskId} = req.params  // destructuring
+        const task = await Task.findOneAndDelete({_id: taskId})
+        if(!task){
+            return res.status(404).send({msg: 'No such task with id: ' + taskId + ' exists'})
+        }
+        res.status(200).send(task)
+    }
+    catch(error){
+        res.status(500).send({msg: error})
+    }
 }
 
 module.exports = { getAllTasks, addTask, getTask, updateTask, deleteTask}
